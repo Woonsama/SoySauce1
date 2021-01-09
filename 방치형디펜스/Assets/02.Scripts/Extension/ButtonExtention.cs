ecd8f7b8d62522a9d3a1194d289cee4c8546602c
Woonsama <kws4510@naver.com>
2021-01-09 20:02:17 +0900

    버튼 확장 추가

---
+++ 방치형디펜스/Assets/02.Scripts/Extension/ButtonExtention.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ButtonExtention : Button
+{
+    private Button button;
+
+    //버튼 클릭 시에 이벤트 함수 추가용
+    public UnityAction action;
+
+    protected override void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick?.AddListener(action);
+    }
+}
