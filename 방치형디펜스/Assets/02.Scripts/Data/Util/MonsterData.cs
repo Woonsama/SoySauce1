dc0ac5d0523b0b727dbf6c0f8bd8a48342bf57b2
Woonsama <kws4510@naver.com>
2021-03-07 15:15:44 +0900

    (data) 몬스터 데이터 추가

---
+++ 방치형디펜스/Assets/02.Scripts/Data/Util/MonsterData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class MonsterData
+{
+    public string nameKr;
+    public string nameEn;
+    public int hp;
+    public int attackPower;
+    public float speed;
+    public float attackDelay;
+
+    public MonsterData(string nameKr, string nameEn, int hp, int attackPower, float speed, float attackDelay)
+    {
+        this.nameKr = nameKr;
+        this.nameEn = nameEn;
+        this.hp = hp;
+        this.attackPower = attackPower;
+        this.speed = speed;
+        this.attackDelay = attackDelay;
+    }
+}
